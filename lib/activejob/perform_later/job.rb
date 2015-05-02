17afa07f48ce62a952a7512f32dc1a4dc069e9dd
Cristian Bica <cristian.bica@gmail.com>
2015-05-02 08:34:25 +0300

    Fixed calling instance method

--- lib/activejob/perform_later/job.rb
+++ lib/activejob/perform_later/job.rb
@@ -3,7 +3,7 @@ module Activejob
     class Job < ActiveJob::Base
       def perform(target, method_name, args)
         target = target.safe_constantize if target.is_a?(String)
-        target.public_send method_name, args
+        target.public_send method_name, *args
       end
     end
   end
--- lib/activejob/perform_later/version.rb
+++ lib/activejob/perform_later/version.rb
@@ -1,5 +1,5 @@
 module Activejob
   module PerformLater
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
   end
 end
