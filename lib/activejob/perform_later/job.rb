081c5765700e1848a9c3cd2bad61fd03e5fbf569
kaspernj <k@spernj.org>
2021-12-31 15:34:22 +0000

    Support Ruby 3 keyword arguments

--- lib/activejob/perform_later/job.rb
+++ lib/activejob/perform_later/job.rb
@@ -1,9 +1,9 @@
 module Activejob
   module PerformLater
     class Job < ActiveJob::Base
-      def perform(target, method_name, args)
+      def perform(target, method_name, args, opts)
         target = target.safe_constantize if target.is_a?(String)
-        target.public_send method_name, *args
+        target.public_send method_name, *args, **opts
       end
     end
   end
